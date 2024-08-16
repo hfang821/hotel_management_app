a595a6f39193c34820918b0df91b45f638d57800
Kevin Fang <kevin-fang@hotmail.ca>
2024-08-15 23:44:52 -0400

    add reservations to the html file to display them

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
 
 @Component({
   selector: 'app-reservation-list',
   templateUrl: './reservation-list.component.html',
   styleUrls: ['./reservation-list.component.css']
 })
-export class ReservationListComponent {
+// use the OnInit lifecycle hook
+export class ReservationListComponent implements OnInit{
+  // create the reservation property
+  reservations: Reservation[] = []
 
+  // create a constructor to inject the ReservationService
+  constructor(private reservationService: ReservationService){}
+
+  ngOnInit(): void {
+    // load the reservations from the service and put them in the Reservation property
+    this.reservations = this.reservationService.getReservations();
+  }
 }
