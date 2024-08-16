2c9f128a76a0e7cca6c328ae2e9f77b42b4203da
Kevin Fang <kevin-fang@hotmail.ca>
2024-08-16 13:47:06 -0400

    add router module and the edit route to edit the reservation

--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
 import { ReservationListComponent } from '../reservation-list/reservation-list.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    RouterModule
   ]
 })
 export class ReservationModule {}
